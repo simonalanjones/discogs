6d7b093dc779911bdb32ff6e82714a5f73970ad7
Simon Jones <simonajones@gmail.com>
2023-09-27 23:21:54 +0100

    clean up

--- app/discogs/Release.jsx
+++
@@ -1,6 +0,0 @@
-async function getReleases() {
-  const res = await fetch(`https://api.discogs.com/releases/6279933`);
-  const release = await res.json();
-  //console.log(release);
-  return release;
-}
--- app/page.jsx
+++ app/page.jsx
@@ -5,14 +5,6 @@ import Loading from "./loading";
 export default function Releases() {
   return (
     <main>
-      <nav>
-        <div>
-          <h2>Releases</h2>
-          <p>
-            <small>My record collection</small>
-          </p>
-        </div>
-      </nav>
       <Suspense fallback={<Loading />}>
         <ReleaseList />
       </Suspense>
